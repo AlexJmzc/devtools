34a3250f029ced46ac27961f017c68ec11fceed4
AlexJmzc <73499509+AlexJmzc@users.noreply.github.com>
2024-05-01 11:08:45 -0500

    Creators data and image

---
+++ src/assets/creators/data.jsx
@@ -0,0 +1,50 @@
+/* IMAGES */
+import midudevImg from "./img/midudev.png";
+
+
+export const CreatorsData = [
+    {
+        id: 1,
+        name: "Midudev",
+        youtubeURL: "",
+        twitchURL: "",
+        twitterURL: "",
+        tiktokURL: "",
+        instagramURL: "",
+        imageURL: midudevImg,
+        description: ""
+    },
+    {
+        id: 2,
+        name: "Midudev",
+        youtubeURL: "",
+        twitchURL: "",
+        twitterURL: "",
+        tiktokURL: "",
+        instagramURL: "",
+        imageURL: midudevImg,
+        description: ""
+    },/*
+    {
+        id: 3,
+        name: "Midudev",
+        youtubeURL: "",
+        twitchURL: "",
+        twitterURL: "",
+        tiktokURL: "",
+        instagramURL: "",
+        imageURL: "",
+        description: ""
+    },
+    {
+        id: 4,
+        name: "Midudev",
+        youtubeURL: "",
+        twitchURL: "",
+        twitterURL: "",
+        tiktokURL: "",
+        instagramURL: "",
+        imageURL: "",
+        description: ""
+    }*/
+]
