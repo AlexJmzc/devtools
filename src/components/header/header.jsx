6d5e4741ab51d040578d239f61867e0f489f3f9f
AlexJmzc <73499509+AlexJmzc@users.noreply.github.com>
2024-05-03 12:40:59 -0500

    BrowserRouter install and routes define

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,28 +2,33 @@ import React from "react";
 import Logo from "../../assets/img/LogoDT.png";
 
 const Header = () => {
+
+  const redirect = (url) => {
+    window.location.href= url;
+  }
+  
   return (
     <>
       <header className="bg-primary h-20 text-center flex items-center justify-around text-quaternary  ">
-        <div className="flex items-center gap-x-2">
+        <div className="flex items-center gap-x-2 cursor-pointer" onClick={() => redirect("/")}>
           <img className="h-8 w-8" src={Logo} alt="DevTools"/>
           <h1 className="font-bold">DEV TOOLS</h1>
         </div>
 
         <ul className="flex gap-x-6">
-          <li className="flex items-center gap-x-1">
+          <li className="flex items-center gap-x-1 cursor-pointer" onClick={() => redirect("/colors")}>
             <span className="icon-[carbon--color-palette]"></span>
             <a href="/color">Paletas de colores</a>
           </li>
-          <li className="flex items-center gap-x-1">
+          <li className="flex items-center gap-x-1 cursor-pointer" onClick={() => redirect("/fonts")}>
             <span className="icon-[ph--pen-nib-duotone]"></span>
             <a href="/fonts">Fuentes</a>
           </li>
-          <li className="flex items-center gap-x-1">
+          <li className="flex items-center gap-x-1 cursor-pointer" onClick={() => redirect("/textures")}>
             <span className="icon-[material-symbols--texture-sharp]"></span>
             <a href="/textures">Texturas</a>
           </li>
-          <li className="flex items-center gap-x-1">
+          <li className="flex items-center gap-x-1 cursor-pointer" onClick={() => redirect("/creators")}>
             <span className="icon-[material-symbols-light--video-library-rounded]"></span>
             <a href="/creators">Creadores de contenido</a>
           </li>
